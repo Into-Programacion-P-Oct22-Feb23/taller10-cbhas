fff144e7344ebe5eaf327bdbd1e5adf7a1ffbde7
Sebastián <calderonsebastian211@gmail.com>
2022-12-23 10:56:20 -0500

    Avance Taller10

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
 
-/**
- *
- * @author reroes
+/*
+ * @author ALISrj21 & cbhas
  */
+
 public class Problema01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
+        String[] nombresEstudiante = {"Jerry Ponce", "Gabriela Lewis", "David Bell"};
+        double[] sumaNotas = new double[3];
+
+        double suma = 0;
+        String reporte = "";
+
+        for (int i = 0; i < notas.length; i++) {
+            suma = 0;
+            for (int j = 0; j < notas[i].length; j++) {
+                suma = suma + notas[i][j];
+
+            }
+            sumaNotas[i] = suma / notas[i].length; // 
+        }
+
+        for (int i = 0; i < nombresEstudiante.length; i++) {
+            reporte = String.format("%sEstudiante: %s tiene un proemdio de "
+                    + "%.2f\n",
+                    reporte,
+                    nombresEstudiante[i],
+                    sumaNotas[i]);
+        }
+
+        System.out.printf("%s\n", reporte);
+
     }
-    
 }
--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
 
-/**
- *
- * @author reroes
+import java.util.Scanner;
+import java.util.Locale;
+
+/*
+ * @author ALISrj21 & cbhas
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int[][] ventas = new int[2][5];
+        String[] vendedores = {"Jessica Cole", "Robert Wallace"};
+        int suma = 0;
+
+        for (int i = 0; i < vendedores.length; i++) {
+            for (int j = 0; j < ventas[i].length; j++) {
+                System.out.println("Ingrese el número de venta del día: ");
+                ventas[i][j] = entrada.nextInt();
+
+                suma = suma + ventas[i][j];
+
+            }
+
+        }
+        for (int i = 0; i < vendedores.length; i++) {
+            System.out.printf("Vendedor(a) %s\n", vendedores[i]);
+
+        }
+        System.out.printf("Ha realizado un total de %d en ventas.\n", suma);
+
+    }
 }
